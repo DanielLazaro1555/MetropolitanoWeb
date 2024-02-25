b54e1ff4e0a7ee0cb5964f95fd02decd5a9b3f94
danielhuaman <danielsam_m@hotmail.com>
2024-02-24 21:24:08 -0500

    Nuevo dato

--- Estaciones.js
+++ Estaciones.js
@@ -1,7 +1,7 @@
 function convertirNumerosAPalabras(texto) {
   const numerosAPalabras = {
     0: "Los Incas",
-    1: "uno",
+    1: "Andrés Belaunde",
     2: "dos",
     3: "tres",
     4: "cuatro",
@@ -62,8 +62,39 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                 </div>`;
           break;
-        case "andrés belaunde":
-          mensaje.innerHTML = "<p>Mensaje para Andrés Belaunde</p>";
+        case "Andrés Belaunde":
+          mensaje.innerHTML = `<div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="contenedor">
+                            <h5 class="card-title">Regular B</h5>
+                            <img src="Vehiculo_Mesa de trabajo 1.svg" alt="30px" style="max-width: 100px; height: auto;">
+                            <div class="horario">
+                                <p class="card-text">Horario</p>
+                                <p class="card-text">Lunes a viernes: <br> 05:00 a. m. - 10:00 a. m.</p>
+                                <p class="card-text">Sábado: <br> 5:00 a. m. - 11:00 p. m.</p>
+                                <p class="card-text">Domingo: <br> 5:00 a. m. - 10:00 p. m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="contenedor">
+                            <h5 class="card-title">Expreso 1</h5>
+                            <img src="Vehiculo_Mesa de trabajo 1.svg" alt="30px" style="max-width: 100px; height: auto;">
+                            <div class="horario">
+                                <p class="card-text">Horario</p>
+                                <p class="card-text">Lunes a viernes: <br> 05:00 a. m. - 10:00 a. m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
           break;
         case "otra estación":
           mensaje.innerHTML = "<p>Mensaje para Otra Estación</p>";
